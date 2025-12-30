a0cda3b326733cc8bc68596926559d32dcb31906
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2025-12-30 17:11:27 +0900

    ボスエネミーを一旦追加

---
+++ Game/Source/Actor/BossEnemy.cpp
@@ -0,0 +1,202 @@
+﻿#include "stdafx.h"
+#include "BossEnemy.h"
+#include "BossEnemyStateMachine.h"
+#include "Source/Collision/CollisionManager.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		// ヘッダーのstatic宣言を消し、これをコンストラクタで定義すれば、同じクラスを使っても違うPLAYER_ANIMATION_OPTIONSを設定できる。
+		// ただ、staticの方がメモリ効率は良いので今回はこの形。
+		const Character::AnimationOption BossEnemy::BOSS_ENEMY_ANIMATION_OPTIONS[] = {
+		   {"Bear/idle",	true},
+		   {"Bear/walk",	true},
+		   {"Bear/run",		true},
+		   {"Bear/attack",	false},
+		   {"Bear/damage",	false},
+		   {"Bear/dead",	false},
+		};
+
+		namespace
+		{
+			const std::string MODEL_PATH = "Bear/bear";
+			constexpr float MODEL_SCALE = 200.0f;
+
+			constexpr int MAX_LIFE = 10;										// 最大体力。
+
+			constexpr float COLLIDER_OFFSET = 100.0f;							// ボディコライダーのオフセット値。
+			constexpr float HIT_COLLIDER_RADIUS = 100.0f;						// 当たりコライダーのサイズ。
+			constexpr float HURT_COLLIDER_RADIUS = 300.0f;						// やられコライダーのサイズ。
+
+			const Vector3 SPAWN_POSITION = Vector3(0.0f, 0.0f, 2000.0f);		// スポーン座標。
+
+			constexpr float COOLDOWN_DURATION = 3.0f;							// 攻撃のクールダウン時間。
+			constexpr float ATTACK_RANGE = 300.0f;								// 攻撃範囲。
+			constexpr float ATTACK_RADIUS = 200.0f;								// 攻撃判定の半径。
+		}
+
+		BossEnemy::BossEnemy()
+		{
+			m_stateMachine = std::make_unique<app::bossEnemy::BossEnemyStateMachine>(this);
+		}
+
+		BossEnemy::~BossEnemy()
+		{
+		}
+
+
+		/// <summary>
+		/// プレイヤーに向かって走ります。
+		/// </summary>
+		void BossEnemy::ChasePlayer(const float speed)
+		{
+			// 水平方向に速度加算。
+			m_moveSpeed += CalcHorizontalVelocity(speed);
+
+			// 垂直方向に速度加算。
+			m_moveSpeed += CalcVerticalVelocity();
+
+			// 移動速度から座標更新。
+			ComputePosition();
+
+			// 攻撃方向を設定。
+			SetAttackDirection(m_moveSpeed);
+		}
+
+		void BossEnemy::UpdateCooldown()
+		{
+			m_cooldownTimer -= g_gameTime->GetFrameDeltaTime();
+		}
+
+		const bool BossEnemy::GetIsOnCooldown()const
+		{
+			return m_cooldownTimer > 0.0f;
+		}
+
+		const void BossEnemy::ResetCooldownTimer()
+		{
+			m_cooldownTimer = COOLDOWN_DURATION;
+		}
+
+		const float BossEnemy::GetDistanceToPlayer()const
+		{
+			Vector3 distance = m_playerFoundPos - m_position;
+			return distance.Length();
+		}
+
+
+		void BossEnemy::OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName)
+		{
+			//キーの名前が「attack_start」の時。
+			if (wcscmp(eventName, L"attack_start") == 0)
+			{
+				SoundManager::Play(enSoundList_BossAttack, false, true, m_position);
+
+				if (CollisionHitManager::GetInstance() == nullptr) {
+					return;
+				}
+
+				m_attackCollider = CollisionHitManager::GetInstance()->CreateCollider(
+					this,
+					enCollisionType_BossEnemy,
+					ATTACK_RADIUS,
+					app::EnCollisionAttr::enCollisionAttr_Enemy
+				);
+
+				Vector3 attackPosition = m_position + m_attackDirection * ATTACK_RANGE;
+				m_attackCollider->SetPosition(attackPosition);
+			}
+			//キーの名前が「attack_end」の時。
+			else if (wcscmp(eventName, L"attack_end") == 0)
+			{
+				if (CollisionHitManager::GetInstance() == nullptr) {
+					return;
+				}
+
+				//攻撃用コライダーを削除。
+				m_attackCollider = CollisionHitManager::GetInstance()->DeleteCollider(m_attackCollider);
+			}
+			//キーの名前が「step」の時。
+			else if (wcscmp(eventName, L"step") == 0)
+			{
+				SoundManager::GetInstance()->Play(enSoundList_BossStep);
+			}
+			else if (wcscmp(eventName, L"dead") == 0) {
+				SoundManager::Play(enSoundList_BossDead, false, true, m_position);
+			}
+		}
+
+
+		bool BossEnemy::Start()
+		{
+			// モデルとアニメーションを初期化。
+			InitModel(enAnimationClip_Num, BOSS_ENEMY_ANIMATION_OPTIONS, MODEL_PATH, MODEL_SCALE);
+
+			// 星に埋もれないように初期位置を調整。
+			m_position = SPAWN_POSITION;
+
+			ResetRotation();
+
+			m_maxLife = MAX_LIFE;
+			InitLife(m_maxLife);
+
+			// 初期ステートを設定
+			m_stateMachine->InitializeState(enBossEnemyState_Idle);
+
+			// 攻撃判定のコライダーを作成。
+			m_hitCollider = CollisionHitManager::GetInstance()->CreateCollider(
+				this,
+				enCollisionType_BossEnemy,
+				HIT_COLLIDER_RADIUS,
+				app::EnCollisionAttr::enCollisionAttr_Enemy
+			);
+			// やられ判定のコライダーを作成。
+			m_hurtCollider = CollisionHitManager::GetInstance()->CreateCollider(
+				this,
+				enCollisionType_BossEnemy,
+				HURT_COLLIDER_RADIUS,
+				app::EnCollisionAttr::enCollisionAttr_Enemy
+			);
+
+			//アニメーションイベント用の関数を設定する。
+			m_modelRender.AddAnimationEvent([&](const wchar_t* clipName, const wchar_t* eventName) {
+				OnAnimationEvent(clipName, eventName);
+				});
+
+			return true;
+		}
+
+		void BossEnemy::Update()
+		{
+			// ポーズ中または戦闘終了時は更新しない。
+			if (BattleManager::GetIsBattleFinish()) {
+				return;
+			}
+
+			m_moveSpeed = Vector3::Zero;
+
+			//「惑星の中心→キャラ」のベクトルを計算し、正規化します。
+			UpdateUpDirection();
+
+			m_stateMachine->Update();
+
+			CollisionHitManager::GetInstance()->UpdateCollider(this, m_hitCollider, COLLIDER_OFFSET);
+			CollisionHitManager::GetInstance()->UpdateCollider(this, m_hurtCollider, COLLIDER_OFFSET);
+
+			m_modelRender.SetPosition(m_position);
+			m_modelRender.Update();
+
+			if (m_life <= 0) {
+				SetIsDying(true);
+			}
+
+		}
+
+		void BossEnemy::Render(RenderContext& rc)
+		{
+			m_modelRender.Draw(rc);
+		}
+	}
+}
---
+++ Game/Source/Actor/BossEnemy.h
@@ -0,0 +1,96 @@
+﻿/// <summary>
+/// エネミーを管理するクラス。
+/// </summary>
+#pragma once
+#include "Enemy.h"
+
+
+namespace app
+{
+	namespace actor
+	{
+		class BossEnemy : public Enemy
+		{
+		public:
+			/// <summary>
+			/// アニメーションのクリップを表す列挙型です。
+			/// StateMachineでアニメを切り替えるために使用します。
+			/// </summary>
+			enum EnAnimationClip
+			{
+				enAnimationClip_Idle,	// 待機アニメーション。
+				enAnimationClip_Walk,	// 待機アニメーション。
+				enAnimationClip_Run,	// 走りアニメーション。
+				enAnimationClip_Attack,	// 攻撃アニメーション。
+				enAnimationClip_Damage,	// ダメージアニメーション。
+				enAnimationClip_Dead,	// 死亡アニメーション。
+				enAnimationClip_Num,
+			};
+
+
+		public:
+			/// <summary>
+			/// 最大ライフを取得します。
+			/// </summary>
+			/// <returns> 最大ライフ。</returns>
+			int GetMaxLife() const
+			{
+				return m_maxLife;
+			}
+
+			/// <summary>
+			/// プレイヤーに向かって走ります。
+			/// </summary>
+			/// <param name="speed">移動速度。</param>
+			void ChasePlayer(const float speed);
+
+			/// <summary>
+			/// クールダウンを更新します。
+			/// </summary>
+			void UpdateCooldown();
+			/// <summary>
+			/// 現在クールダウン中かどうかを判定します。
+			/// </summary>
+			/// <returns>クールダウン中であればtrue、そうでなければfalseを返します。</returns>
+			const bool GetIsOnCooldown()const;
+			/// <summary>
+			/// クールダウンタイマーをリセットします。
+			/// </summary>
+			const void ResetCooldownTimer();
+
+			/// <summary>
+			/// プレイヤーまでの距離を計算して返します。const メンバー関数であり、オブジェクトの状態は変更されません。
+			/// </summary>
+			/// <returns>プレイヤーまでの距離を表す float 値を返します（戻り値は const）。</returns>
+			const float GetDistanceToPlayer()const;
+
+			/// <summary>
+			/// アニメーションイベントを処理します。
+			/// </summary>
+			void OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName) override final;
+
+
+		public:
+			BossEnemy();
+			~BossEnemy();
+
+
+		private:
+			Vector3 m_lastPlayerPos = Vector3::Zero;	// 最後にプレイヤーを見た位置。
+			float m_cooldownTimer = 0.0f;				// クールダウンタイマー。
+			int m_maxLife = 0;							// 最大体力。
+
+			// ボスエネミーのステートマシン。
+			std::unique_ptr<app::actor::BossEnemyStateMachine> m_stateMachine;
+
+			// クラススコープで宣言し、cppで定義。
+			static const Character::AnimationOption BOSS_ENEMY_ANIMATION_OPTIONS[];
+
+
+		private:
+			bool Start() override final;
+			void Update() override final;
+			void Render(RenderContext& rc) override final;
+		};
+	}
+}
