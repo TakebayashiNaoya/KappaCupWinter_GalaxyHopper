8e7dbdafc9095568b44342ef0f71956ace040af3
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-01-16 09:35:55 +0900

    名前空間の追加

--- Game/Load/LoadManager.cpp
+++ Game/Load/LoadManager.cpp
@@ -4,296 +4,299 @@
 
 namespace app
 {
-	namespace
+	namespace load
 	{
-		constexpr float FULL_HD_WIDTH = 1920.0f;								/** フルHDの幅 */
-		constexpr float FULL_HD_HEIGHT = 1080.0f;								/** フルHDの高さ */
-
-		constexpr float HALF_FULL_HD_WIDTH = FULL_HD_WIDTH / 2.0f;				/** フルHDの半分の幅 */
-		constexpr float HALF_FULL_HD_HEIGHT = FULL_HD_HEIGHT / 2.0f;			/** フルHDの半分の高さ */
-
-		constexpr float CENTER_IMAGE_RATIO = 5.0f;								/** 中央画像の倍率 */
-
-		const float CENTER_IMAGE_WIDTH = FULL_HD_WIDTH * CENTER_IMAGE_RATIO;	/** 中央画像の幅 */
-		const float CENTER_IMAGE_HEIGHT = FULL_HD_HEIGHT * CENTER_IMAGE_RATIO;	/** 中央画像の高さ */
-
-		const Vector2 PIVOT_TOP_CENTER = { 0.5f, 1.0f };						/** 上辺の中央 */
-		const Vector2 PIVOT_BOTTOM_CENTER = { 0.5f, 0.0f };						/** 下辺の中央 */
-		const Vector2 PIVOT_CENTER_LEFT = { 0.0f, 0.5f };						/** 左辺の中央 */
-		const Vector2 PIVOT_CENTER_RIGHT = { 1.0f, 0.5f };						/** 右辺の中央 */
-		const Vector2 PIVOT_CENTER = { 0.5f, 0.5f };							/** 完全な中央 */
-
-		constexpr float CROSE_DURATION = 1.5f;									/** クローズの時間 */
-		constexpr float OPEN_DURATION = 1.5f;									/** オープンの時間 */
-
-
-		/**
-		 * 線形補間関数(Linear Interpolation)
-		 * a:開始値, b:終了値, t:割合(0.0～1.0)
-		 */
-		float Lerp(float a, float b, float t)
+		namespace
 		{
-			return a + (b - a) * t;
-		}
-	}
+			constexpr float FULL_HD_WIDTH = 1920.0f;								/** フルHDの幅 */
+			constexpr float FULL_HD_HEIGHT = 1080.0f;								/** フルHDの高さ */
+
+			constexpr float HALF_FULL_HD_WIDTH = FULL_HD_WIDTH / 2.0f;				/** フルHDの半分の幅 */
+			constexpr float HALF_FULL_HD_HEIGHT = FULL_HD_HEIGHT / 2.0f;			/** フルHDの半分の高さ */
+
+			constexpr float CENTER_IMAGE_RATIO = 5.0f;								/** 中央画像の倍率 */
+
+			const float CENTER_IMAGE_WIDTH = FULL_HD_WIDTH * CENTER_IMAGE_RATIO;	/** 中央画像の幅 */
+			const float CENTER_IMAGE_HEIGHT = FULL_HD_HEIGHT * CENTER_IMAGE_RATIO;	/** 中央画像の高さ */
+
+			const Vector2 PIVOT_TOP_CENTER = { 0.5f, 1.0f };						/** 上辺の中央 */
+			const Vector2 PIVOT_BOTTOM_CENTER = { 0.5f, 0.0f };						/** 下辺の中央 */
+			const Vector2 PIVOT_CENTER_LEFT = { 0.0f, 0.5f };						/** 左辺の中央 */
+			const Vector2 PIVOT_CENTER_RIGHT = { 1.0f, 0.5f };						/** 右辺の中央 */
+			const Vector2 PIVOT_CENTER = { 0.5f, 0.5f };							/** 完全な中央 */
+
+			constexpr float CROSE_DURATION = 1.5f;									/** クローズの時間 */
+			constexpr float OPEN_DURATION = 1.5f;									/** オープンの時間 */
 
 
-	LoadManager* LoadManager::m_instance = nullptr;
-
-
-	void LoadManager::StartLoading()
-	{
-		if (m_instance != nullptr) {
-			m_instance->m_state = EnState::Closing;
-
-			/** ロード画面が閉まり始めるタイミングで、全BGMをフェードアウトさせる */
-			sound::SoundManager::StopAllBGM(CROSE_DURATION);
-		}
-	}
-
-
-	void LoadManager::FinishLoading()
-	{
-		if (m_instance != nullptr) {
-			m_instance->m_state = EnState::Opening;
-
-			/** ロード明けのアニメーション時間に合わせてフェードインさせる */
-			sound::SoundManager::FadeInAllBGM(OPEN_DURATION);
-		}
-	}
-
-
-	void LoadManager::SnapToLoading()
-	{
-		UpdateIrisTransform(0.0f);
-		sound::SoundManager::StopAllBGM(0.0f);
-		m_state = EnState::Loading;
-	}
-
-
-	void LoadManager::SnapToOpened()
-	{
-		UpdateIrisTransform(1.0f);
-		m_state = EnState::Opened;
-	}
-
-
-	void LoadManager::ChangeState(EnState state)
-	{
-		if (m_instance == nullptr) {
-			return;
-		}
-
-		m_instance->m_state = state;
-		m_instance->m_timer = 0.0f;
-
-		// ローディング画面レイアウトに強制変更。
-		if (state == EnState::Loading) {
-			m_instance->SnapToLoading();
-			sound::SoundManager::StopAllBGM(0.0f);
-		}
-		else if (state == EnState::Opened) {
-			m_instance->SnapToOpened();
-		}
-	}
-
-
-	const LoadManager::EnState LoadManager::GetState()
-	{
-		if (m_instance != nullptr) {
-			return m_instance->m_state;
-		}
-		return EnState::None;
-	}
-
-
-	LoadManager::LoadManager()
-	{
-	}
-
-
-	LoadManager::~LoadManager()
-	{
-		if (m_displayImages != nullptr) {
-			delete[] m_displayImages;
-			m_displayImages = nullptr;
-		}
-	}
-
-
-	bool LoadManager::Start()
-	{
-		/** レイアウト初期化 */
-		InitLayout();
-
-		return true;
-	}
-
-
-	void LoadManager::Update()
-	{
-		switch (m_state)
-		{
-		case EnState::Closing:
-			UpdateAnimation(1.0f, 0.0f, CROSE_DURATION, EnState::Loading);
-			break;
-
-		case EnState::Loading:
-			break;
-
-		case EnState::Opening:
-			UpdateAnimation(0.0f, 1.0f, OPEN_DURATION, EnState::Opened);
-			break;
-
-		case EnState::Opened:
-			break;
-
-		default:
-			break;
-		}
-	}
-
-
-	void LoadManager::Render(RenderContext& rc)
-	{
-		if (m_displayImages == nullptr) {
-			return;
-		}
-
-		/** 必要なときだけ各画像を描画 */
-		if (m_state == EnState::Closing || m_state == EnState::Opening) {
-			for (uint8_t i = 0; i < enImageParts_Num; ++i) {
-				m_displayImages[i].Draw(rc);
+			/**
+			 * 線形補間関数(Linear Interpolation)
+			 * a:開始値, b:終了値, t:割合(0.0～1.0)
+			 */
+			float Lerp(float a, float b, float t)
+			{
+				return a + (b - a) * t;
 			}
-			return;
 		}
 
-		/** ローディング中はローディングアイコンを描画 */
-		if (m_state == EnState::Loading) {
-			m_loadingIcon.Draw(rc);
-		}
-	}
+
+		LoadManager* LoadManager::m_instance = nullptr;
 
 
-	void LoadManager::InitLayout()
-	{
-		/** スプライトレンダー配列を生成 */
-		m_displayImages = new SpriteRender[enImageParts_Num];
+		void LoadManager::StartLoading()
+		{
+			if (m_instance != nullptr) {
+				m_instance->m_state = EnState::Closing;
 
-
-		/** 各スプライトレンダーを初期化・配置 */
-		m_displayImages[enImageParts_Center].Init("Assets/Sprite/LoadingImage_Center.dds", CENTER_IMAGE_WIDTH, CENTER_IMAGE_HEIGHT);
-
-		m_displayImages[enImageParts_Top].Init("Assets/Sprite/LoadingImage_Around.dds", FULL_HD_WIDTH, HALF_FULL_HD_HEIGHT);
-		m_displayImages[enImageParts_Top].SetPivot(PIVOT_BOTTOM_CENTER);
-		m_displayImages[enImageParts_Top].SetPosition({ 0.0f, HALF_FULL_HD_HEIGHT * CENTER_IMAGE_RATIO, 0.0f });
-
-		m_displayImages[enImageParts_Bottom].Init("Assets/Sprite/LoadingImage_Around.dds", FULL_HD_WIDTH, HALF_FULL_HD_HEIGHT);
-		m_displayImages[enImageParts_Bottom].SetPivot(PIVOT_TOP_CENTER);
-		m_displayImages[enImageParts_Bottom].SetPosition({ 0.0f, HALF_FULL_HD_HEIGHT * CENTER_IMAGE_RATIO * -1.0f, 0.0f });
-
-		m_displayImages[enImageParts_Left].Init("Assets/Sprite/LoadingImage_Around.dds", HALF_FULL_HD_WIDTH, FULL_HD_HEIGHT);
-		m_displayImages[enImageParts_Left].SetPivot(PIVOT_CENTER_RIGHT);
-		m_displayImages[enImageParts_Left].SetPosition({ HALF_FULL_HD_WIDTH * CENTER_IMAGE_RATIO * -1.0f, 0.0f, 0.0f });
-
-		m_displayImages[enImageParts_Right].Init("Assets/Sprite/LoadingImage_Around.dds", HALF_FULL_HD_WIDTH, FULL_HD_HEIGHT);
-		m_displayImages[enImageParts_Right].SetPivot(PIVOT_CENTER_LEFT);
-		m_displayImages[enImageParts_Right].SetPosition({ HALF_FULL_HD_WIDTH * CENTER_IMAGE_RATIO, 0.0f, 0.0f });
-
-		m_loadingIcon.Init("Assets/Sprite/Loading.dds", FULL_HD_WIDTH, FULL_HD_HEIGHT);
-
-
-		/** 画像を更新 */
-		for (uint8_t i = 0; i < enImageParts_Num; ++i) {
-			m_displayImages[i].Update();
+				/** ロード画面が閉まり始めるタイミングで、全BGMをフェードアウトさせる */
+				sound::SoundManager::StopAllBGM(CROSE_DURATION);
+			}
 		}
 
-		/** タイマー初期化 */
-		m_timer = 0.0f;
-	}
 
+		void LoadManager::FinishLoading()
+		{
+			if (m_instance != nullptr) {
+				m_instance->m_state = EnState::Opening;
 
-	void LoadManager::UpdateIrisTransform(float ratio)
-	{
-		/** 中央画像のサイズを設定 */
-		m_displayImages[enImageParts_Center].SetScale({ ratio, ratio, 1.0f });
-
-		/** 中央画像の大きさから周囲画像の位置を計算 */
-		float currentTopPos = (CENTER_IMAGE_HEIGHT * ratio) / 2.0f;
-		float currentBottomPos = currentTopPos * -1.0f;
-		float currentRightPos = (CENTER_IMAGE_WIDTH * ratio) / 2.0f;
-		float currentLeftPos = currentRightPos * -1.0f;
-
-		/** 周囲画像の位置を設定 */
-		m_displayImages[enImageParts_Top].SetPosition({ 0.0f, currentTopPos, 0.0f });
-		m_displayImages[enImageParts_Bottom].SetPosition({ 0.0f, currentBottomPos, 0.0f });
-		m_displayImages[enImageParts_Right].SetPosition({ currentRightPos, 0.0f, 0.0f });
-		m_displayImages[enImageParts_Left].SetPosition({ currentLeftPos, 0.0f, 0.0f });
-
-		/** 全パーツ更新 */
-		for (uint8_t i = 0; i < enImageParts_Num; ++i) {
-			m_displayImages[i].Update();
-		}
-	}
-
-
-	void LoadManager::UpdateAnimation(float startRatio, float endRatio, float duration, EnState nextState)
-	{
-		m_timer += g_gameTime->GetFrameDeltaTime();
-
-		/** 進行度（0.0 ～ 1.0）を計算 */
-		float timer = m_timer / duration;
-
-		/** 1.0を超えないようにクランプ */
-		if (timer > 1.0f) {
-			timer = 1.0f;
+				/** ロード明けのアニメーション時間に合わせてフェードインさせる */
+				sound::SoundManager::FadeInAllBGM(OPEN_DURATION);
+			}
 		}
 
-		/** 補間率を計算 */
-		float ratio = Lerp(startRatio, endRatio, timer);
 
-		/** 計算結果を適用 */
-		UpdateIrisTransform(ratio);
+		void LoadManager::SnapToLoading()
+		{
+			UpdateIrisTransform(0.0f);
+			sound::SoundManager::StopAllBGM(0.0f);
+			m_state = EnState::Loading;
+		}
 
-		/** 時間経過で次のステートへ */
-		if (timer >= 1.0f) {
+
+		void LoadManager::SnapToOpened()
+		{
+			UpdateIrisTransform(1.0f);
+			m_state = EnState::Opened;
+		}
+
+
+		void LoadManager::ChangeState(EnState state)
+		{
+			if (m_instance == nullptr) {
+				return;
+			}
+
+			m_instance->m_state = state;
+			m_instance->m_timer = 0.0f;
+
+			// ローディング画面レイアウトに強制変更。
+			if (state == EnState::Loading) {
+				m_instance->SnapToLoading();
+				sound::SoundManager::StopAllBGM(0.0f);
+			}
+			else if (state == EnState::Opened) {
+				m_instance->SnapToOpened();
+			}
+		}
+
+
+		const LoadManager::EnState LoadManager::GetState()
+		{
+			if (m_instance != nullptr) {
+				return m_instance->m_state;
+			}
+			return EnState::None;
+		}
+
+
+		LoadManager::LoadManager()
+		{
+		}
+
+
+		LoadManager::~LoadManager()
+		{
+			if (m_displayImages != nullptr) {
+				delete[] m_displayImages;
+				m_displayImages = nullptr;
+			}
+		}
+
+
+		bool LoadManager::Start()
+		{
+			/** レイアウト初期化 */
+			InitLayout();
+
+			return true;
+		}
+
+
+		void LoadManager::Update()
+		{
+			switch (m_state)
+			{
+			case EnState::Closing:
+				UpdateAnimation(1.0f, 0.0f, CROSE_DURATION, EnState::Loading);
+				break;
+
+			case EnState::Loading:
+				break;
+
+			case EnState::Opening:
+				UpdateAnimation(0.0f, 1.0f, OPEN_DURATION, EnState::Opened);
+				break;
+
+			case EnState::Opened:
+				break;
+
+			default:
+				break;
+			}
+		}
+
+
+		void LoadManager::Render(RenderContext& rc)
+		{
+			if (m_displayImages == nullptr) {
+				return;
+			}
+
+			/** 必要なときだけ各画像を描画 */
+			if (m_state == EnState::Closing || m_state == EnState::Opening) {
+				for (uint8_t i = 0; i < enImageParts_Num; ++i) {
+					m_displayImages[i].Draw(rc);
+				}
+				return;
+			}
+
+			/** ローディング中はローディングアイコンを描画 */
+			if (m_state == EnState::Loading) {
+				m_loadingIcon.Draw(rc);
+			}
+		}
+
+
+		void LoadManager::InitLayout()
+		{
+			/** スプライトレンダー配列を生成 */
+			m_displayImages = new SpriteRender[enImageParts_Num];
+
+
+			/** 各スプライトレンダーを初期化・配置 */
+			m_displayImages[enImageParts_Center].Init("Assets/Sprite/LoadingImage_Center.dds", CENTER_IMAGE_WIDTH, CENTER_IMAGE_HEIGHT);
+
+			m_displayImages[enImageParts_Top].Init("Assets/Sprite/LoadingImage_Around.dds", FULL_HD_WIDTH, HALF_FULL_HD_HEIGHT);
+			m_displayImages[enImageParts_Top].SetPivot(PIVOT_BOTTOM_CENTER);
+			m_displayImages[enImageParts_Top].SetPosition({ 0.0f, HALF_FULL_HD_HEIGHT * CENTER_IMAGE_RATIO, 0.0f });
+
+			m_displayImages[enImageParts_Bottom].Init("Assets/Sprite/LoadingImage_Around.dds", FULL_HD_WIDTH, HALF_FULL_HD_HEIGHT);
+			m_displayImages[enImageParts_Bottom].SetPivot(PIVOT_TOP_CENTER);
+			m_displayImages[enImageParts_Bottom].SetPosition({ 0.0f, HALF_FULL_HD_HEIGHT * CENTER_IMAGE_RATIO * -1.0f, 0.0f });
+
+			m_displayImages[enImageParts_Left].Init("Assets/Sprite/LoadingImage_Around.dds", HALF_FULL_HD_WIDTH, FULL_HD_HEIGHT);
+			m_displayImages[enImageParts_Left].SetPivot(PIVOT_CENTER_RIGHT);
+			m_displayImages[enImageParts_Left].SetPosition({ HALF_FULL_HD_WIDTH * CENTER_IMAGE_RATIO * -1.0f, 0.0f, 0.0f });
+
+			m_displayImages[enImageParts_Right].Init("Assets/Sprite/LoadingImage_Around.dds", HALF_FULL_HD_WIDTH, FULL_HD_HEIGHT);
+			m_displayImages[enImageParts_Right].SetPivot(PIVOT_CENTER_LEFT);
+			m_displayImages[enImageParts_Right].SetPosition({ HALF_FULL_HD_WIDTH * CENTER_IMAGE_RATIO, 0.0f, 0.0f });
+
+			m_loadingIcon.Init("Assets/Sprite/Loading.dds", FULL_HD_WIDTH, FULL_HD_HEIGHT);
+
+
+			/** 画像を更新 */
+			for (uint8_t i = 0; i < enImageParts_Num; ++i) {
+				m_displayImages[i].Update();
+			}
+
+			/** タイマー初期化 */
 			m_timer = 0.0f;
-			m_state = nextState;
 		}
-	}
+
+
+		void LoadManager::UpdateIrisTransform(float ratio)
+		{
+			/** 中央画像のサイズを設定 */
+			m_displayImages[enImageParts_Center].SetScale({ ratio, ratio, 1.0f });
+
+			/** 中央画像の大きさから周囲画像の位置を計算 */
+			float currentTopPos = (CENTER_IMAGE_HEIGHT * ratio) / 2.0f;
+			float currentBottomPos = currentTopPos * -1.0f;
+			float currentRightPos = (CENTER_IMAGE_WIDTH * ratio) / 2.0f;
+			float currentLeftPos = currentRightPos * -1.0f;
+
+			/** 周囲画像の位置を設定 */
+			m_displayImages[enImageParts_Top].SetPosition({ 0.0f, currentTopPos, 0.0f });
+			m_displayImages[enImageParts_Bottom].SetPosition({ 0.0f, currentBottomPos, 0.0f });
+			m_displayImages[enImageParts_Right].SetPosition({ currentRightPos, 0.0f, 0.0f });
+			m_displayImages[enImageParts_Left].SetPosition({ currentLeftPos, 0.0f, 0.0f });
+
+			/** 全パーツ更新 */
+			for (uint8_t i = 0; i < enImageParts_Num; ++i) {
+				m_displayImages[i].Update();
+			}
+		}
+
+
+		void LoadManager::UpdateAnimation(float startRatio, float endRatio, float duration, EnState nextState)
+		{
+			m_timer += g_gameTime->GetFrameDeltaTime();
+
+			/** 進行度（0.0 ～ 1.0）を計算 */
+			float timer = m_timer / duration;
+
+			/** 1.0を超えないようにクランプ */
+			if (timer > 1.0f) {
+				timer = 1.0f;
+			}
+
+			/** 補間率を計算 */
+			float ratio = Lerp(startRatio, endRatio, timer);
+
+			/** 計算結果を適用 */
+			UpdateIrisTransform(ratio);
+
+			/** 時間経過で次のステートへ */
+			if (timer >= 1.0f) {
+				m_timer = 0.0f;
+				m_state = nextState;
+			}
+		}
 
 
 
 
-	/********************************/
+		/********************************/
 
 
-	LoadManagerObject::LoadManagerObject()
-	{
-		LoadManager::CreateInstance();
-	}
+		LoadManagerObject::LoadManagerObject()
+		{
+			LoadManager::CreateInstance();
+		}
 
 
-	LoadManagerObject::~LoadManagerObject()
-	{
-		LoadManager::Delete();
-	}
+		LoadManagerObject::~LoadManagerObject()
+		{
+			LoadManager::Delete();
+		}
 
 
-	bool LoadManagerObject::Start()
-	{
-		return true;
-	}
+		bool LoadManagerObject::Start()
+		{
+			return true;
+		}
 
 
-	void LoadManagerObject::Update()
-	{
-		LoadManager::GetInstance()->Update();
-	}
+		void LoadManagerObject::Update()
+		{
+			LoadManager::GetInstance()->Update();
+		}
 
 
-	void LoadManagerObject::Render(RenderContext& rc)
-	{
-		LoadManager::GetInstance()->Render(rc);
+		void LoadManagerObject::Render(RenderContext& rc)
+		{
+			LoadManager::GetInstance()->Render(rc);
+		}
 	}
 }
--- Game/Load/LoadManager.h
+++ Game/Load/LoadManager.h
@@ -2,153 +2,156 @@
 
 namespace app
 {
-	class LoadManager
+	namespace load
 	{
-	public:
-		/**
-		 * ロード画面の状態
-		 */
-		enum class EnState : uint8_t
+		class LoadManager
 		{
-			Closing,
-			Loading,
-			Opening,
-			Opened,
-			None
+		public:
+			/**
+			 * ロード画面の状態
+			 */
+			enum class EnState : uint8_t
+			{
+				Closing,
+				Loading,
+				Opening,
+				Opened,
+				None
+			};
+
+			/**
+			 * ロードを開始させる
+			 */
+			static void StartLoading();
+
+			/**
+			 * ロードを終了させる
+			 */
+			static void FinishLoading();
+
+			/**
+			 * ロード状態を強制的にロード中のレイアウトに変更し、全ての音を停止する
+			 */
+			void SnapToLoading();
+
+			/**
+			 * ロード状態を強制的にロード完了のレイアウトに変更する
+			 */
+			void SnapToOpened();
+
+			/**
+			 * ロード状態を強制的に変更する
+			 */
+			static void ChangeState(EnState state);
+
+			/**
+			 * ロード画面の状態を取得する
+			 */
+			static const EnState GetState();
+
+
+		private:
+			/** ロード画面を構成する画像パーツ */
+			enum EnImageParts :uint8_t
+			{
+				enImageParts_Center,	/** 中央 */
+				enImageParts_Top,		/** 上　 */
+				enImageParts_Bottom,	/** 下　 */
+				enImageParts_Left,		/** 左　 */
+				enImageParts_Right,		/** 右　 */
+				enImageParts_Num
+			};
+
+
+		private:
+			/** 画像リスト */
+			SpriteRender* m_displayImages = nullptr;
+			/** ローディングアイコン */
+			SpriteRender m_loadingIcon;
+			/** ロード状態 */
+			EnState m_state = EnState::None;
+			/** アニメーション用タイマー */
+			float m_timer = 0.0f;
+
+
+		private:
+			LoadManager();
+			~LoadManager();
+
+
+		public:
+			bool Start();
+			void Update();
+			void Render(RenderContext& rc);
+
+
+		private:
+			/**
+			 * レイアウトの初期化
+			 */
+			void InitLayout();
+
+			/**
+			 * アイリスイン・アイリスアウト時の各画像の大きさ・座標を更新する
+			 * 引数が0.0fで閉じた状態、1.0fで開いた状態
+			 */
+			void UpdateIrisTransform(float ratio);
+
+			/**
+			 * アニメーションの更新
+			 * startRatio～endRatioまでduration秒かけて変化させ、nextStateに遷移する
+			 */
+			void UpdateAnimation(float startRatio, float endRatio, float duration, EnState nextState);
+
+
+			/**
+			 * シングルトン関連
+			 */
+		private:
+			static LoadManager* m_instance;
+
+		public:
+			static LoadManager* CreateInstance()
+			{
+				if (m_instance == nullptr) {
+					m_instance = new LoadManager();
+				}
+				return m_instance;
+			}
+			static LoadManager* GetInstance()
+			{
+				return m_instance;
+			}
+			static bool GetIsAvailable()
+			{
+				return m_instance != nullptr;
+			}
+			static void Delete()
+			{
+				if (m_instance != nullptr) {
+					delete m_instance;
+					m_instance = nullptr;
+				}
+			}
 		};
 
-		/**
-		 * ロードを開始させる
-		 */
-		static void StartLoading();
-
-		/**
-		 * ロードを終了させる
-		 */
-		static void FinishLoading();
-
-		/**
-		 * ロード状態を強制的にロード中のレイアウトに変更し、全ての音を停止する
-		 */
-		void SnapToLoading();
-
-		/**
-		 * ロード状態を強制的にロード完了のレイアウトに変更する
-		 */
-		void SnapToOpened();
-
-		/**
-		 * ロード状態を強制的に変更する
-		 */
-		static void ChangeState(EnState state);
-
-		/**
-		 * ロード画面の状態を取得する
-		 */
-		static const EnState GetState();
 
 
-	private:
-		/** ロード画面を構成する画像パーツ */
-		enum EnImageParts :uint8_t
+
+		/********************************/
+
+
+		class LoadManagerObject : public IGameObject
 		{
-			enImageParts_Center,	/** 中央 */
-			enImageParts_Top,		/** 上　 */
-			enImageParts_Bottom,	/** 下　 */
-			enImageParts_Left,		/** 左　 */
-			enImageParts_Right,		/** 右　 */
-			enImageParts_Num
+		public:
+			LoadManagerObject();
+			~LoadManagerObject();
+
+
+		private:
+			bool Start() override;
+			void Update() override;
+			void Render(RenderContext& rc) override;
 		};
-
-
-	private:
-		/** 画像リスト */
-		SpriteRender* m_displayImages = nullptr;
-		/** ローディングアイコン */
-		SpriteRender m_loadingIcon;
-		/** ロード状態 */
-		EnState m_state = EnState::None;
-		/** アニメーション用タイマー */
-		float m_timer = 0.0f;
-
-
-	private:
-		LoadManager();
-		~LoadManager();
-
-
-	public:
-		bool Start();
-		void Update();
-		void Render(RenderContext& rc);
-
-
-	private:
-		/**
-		 * レイアウトの初期化
-		 */
-		void InitLayout();
-
-		/**
-		 * アイリスイン・アイリスアウト時の各画像の大きさ・座標を更新する
-		 * 引数が0.0fで閉じた状態、1.0fで開いた状態
-		 */
-		void UpdateIrisTransform(float ratio);
-
-		/**
-		 * アニメーションの更新
-		 * startRatio～endRatioまでduration秒かけて変化させ、nextStateに遷移する
-		 */
-		void UpdateAnimation(float startRatio, float endRatio, float duration, EnState nextState);
-
-
-		/**
-		 * シングルトン関連
-		 */
-	private:
-		static LoadManager* m_instance;
-
-	public:
-		static LoadManager* CreateInstance()
-		{
-			if (m_instance == nullptr) {
-				m_instance = new LoadManager();
-			}
-			return m_instance;
-		}
-		static LoadManager* GetInstance()
-		{
-			return m_instance;
-		}
-		static bool GetIsAvailable()
-		{
-			return m_instance != nullptr;
-		}
-		static void Delete()
-		{
-			if (m_instance != nullptr) {
-				delete m_instance;
-				m_instance = nullptr;
-			}
-		}
-	};
-
-
-
-
-	/********************************/
-
-
-	class LoadManagerObject : public IGameObject
-	{
-	public:
-		LoadManagerObject();
-		~LoadManagerObject();
-
-
-	private:
-		bool Start() override;
-		void Update() override;
-		void Render(RenderContext& rc) override;
-	};
+	}
 }
