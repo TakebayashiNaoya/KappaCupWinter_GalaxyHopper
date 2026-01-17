cfd1846dfb759959c129cb0506dad72727314002
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-01-17 10:43:14 +0900

    enumの命名変更

--- Game/Source/Actor/Character/Player/PlayerStateMachine.cpp
+++ Game/Source/Actor/Character/Player/PlayerStateMachine.cpp
@@ -14,17 +14,17 @@ namespace app
 			:CharacterStateMachine(owner)
 		{
 			/** ステートの生成 */
-			AddState<PlayerIdleState>(Idle);
-			AddState<PlayerWalkState>(Walk);
-			AddState<PlayerDashState>(Dash);
-			AddState<PlayerJumpState>(Jump);
-			AddState<PlayerDamageState>(Damage);
-			AddState<PlayerDyingState>(Dying);
-			AddState<PlayerDeadState>(Dead);
-			K2_ASSERT(m_stateMap.size() == Num, "AddStateを呼んでください");
+			AddState<PlayerIdleState>(enPlayerState_Idle);
+			AddState<PlayerWalkState>(enPlayerState_Walk);
+			AddState<PlayerDashState>(enPlayerState_Dash);
+			AddState<PlayerJumpState>(enPlayerState_Jump);
+			AddState<PlayerDamageState>(enPlayerState_Damage);
+			AddState<PlayerDyingState>(enPlayerState_Dying);
+			AddState<PlayerDeadState>(enPlayerState_Dead);
+			K2_ASSERT(m_stateMap.size() == enPlayerState_Num, "AddStateを呼んでください");
 
 			/** 初期ステート */
-			m_currentState = m_stateMap[Idle];
+			m_currentState = m_stateMap[enPlayerState_Idle];
 		}
 
 
@@ -37,44 +37,44 @@ namespace app
 		{
 			/** ダメージを受けたらダメージ状態へ */
 			if (CanChangeDamage()) {
-				return FindState(Damage);
+				return FindState(enPlayerState_Damage);
 			}
 			/** 条件が満たされるまでダメージ状態を維持 */
-			if (IsEqualCurrentState(Damage)) {
+			if (IsEqualCurrentState(enPlayerState_Damage)) {
 				if (!IsDamageStateFinished()) {
 					return nullptr;
 				}
 			}
 
 			/** 死亡中ならアニメーションが終わるまでステートを維持し、終わったら完全死亡状態へ */
-			if (IsEqualCurrentState(Dying)) {
+			if (IsEqualCurrentState(enPlayerState_Dying)) {
 				if (CanChangeDead()) {
-					return FindState(Dead);
+					return FindState(enPlayerState_Dead);
 				}
 			}
 
 			/** HPが0になっていたら死亡開始状態へ */
 			if (CanChangeDying()) {
-				return FindState(Dying);
+				return FindState(enPlayerState_Dying);
 			}
 
 			/** 足が着いていなければジャンプ状態へ */
 			if (CanChangeJump()) {
-				return FindState(Jump);
+				return FindState(enPlayerState_Jump);
 			}
 
 			/** ダッシュ入力があり、かつ移動入力があるならダッシュ状態へ */
 			if (CanChangeDush()) {
-				return FindState(Dash);
+				return FindState(enPlayerState_Dash);
 			}
 
 			/** 移動入力があるなら歩き状態へ */
 			if (CanChangeWalk()) {
-				return FindState(Walk);
+				return FindState(enPlayerState_Walk);
 			}
 
 			/** どれにも当てはまらなければ待機状態へ */
-			return FindState(Idle);
+			return FindState(enPlayerState_Idle);
 		}
 
 
